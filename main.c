8ddd92d83e01259e13eb0575c185456e811485e1
rizanw <rzkandre@gmail.com>
2019-05-10 15:37:32 +0700

    test

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syslog.h>
+#include <limits.h>
+
