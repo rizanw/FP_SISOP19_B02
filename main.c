58cfe75077abbdb629d7fda13758c9554742357f
rizanw <rzkandre@gmail.com>
2019-05-11 15:13:11 +0700

    daemon

--- main.c
+++ main.c
@@ -1,12 +1,42 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <time.h>
-#include <string.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <unistd.h>
 #include <syslog.h>
-#include <limits.h>
+#include <string.h>
 
+int main() {
+    pid_t pid, sid;
+
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+    
+    umask(0);
+
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if ((chdir("/")) < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    while(1) {
+
+        sleep(30);
+    }
+
+    exit(EXIT_SUCCESS);
+}
